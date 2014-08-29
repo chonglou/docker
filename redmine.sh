d162cee1db435d76e06b2c56949ed6815a18d2a5
flamen <jitang.zheng@gmail.com>
2014-08-28 20:32:24 -0700

    fix start bug

--- redmine.sh
+++ redmine.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 hash docker &>/dev/null || {
-	type yum >/dev/null 2>&1 && yum -y install docker
-	type apt-get >/dev/null 2>&1 && apt-get -y install docker
+	type yum >/dev/null 2>&1 && sudo yum -y install docker
+	type apt-get >/dev/null 2>&1 && sudo apt-get -y install docker
 }
 
 type docker >/dev/null 2>&1 || {
 	echo 'Unknown linux distribution'
 	exit 1
 }
-docker run -p 443:443 -P –privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro chonglou/redmine:latest /sbin/init
+sudo docker run -d -p 443:443 -P --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro chonglou/redmine:latest /sbin/init
